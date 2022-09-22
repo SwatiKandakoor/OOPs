bee98633b9c91c62b56fd894f6b3f7c78dd3a310
swatik.xworkz@gmail.com <swatik.xworkz@gmail.com>
2022-09-22 07:07:06 +0530

    set and get file

---
+++ ToothPaste.java
@@ -0,0 +1,129 @@
+class ToothPaste{
+	static double price=200d;
+	static String flavors="maxFresh";
+	static int water=20;
+	static String fluoride="sodium";
+	static int weight=800;
+	static int clove=50;
+	static int neem=20;
+	static float pudina=1.15f;
+	static float mint=2.24f;
+	static int aloevera=20;
+	static byte tulsi=30;
+	static int salt=25;
+	
+	static void setPrice(double value){
+		price=value;
+		System.out.println(price);
+	}
+	
+	static double getPrice(){
+		return (price);
+	}
+}
+	static void setflavors(String value){
+		flavors=value;
+		System.out.println(flavors);
+	}
+	
+	static String getflavors(){
+		return ("colgate");
+	}
+	
+	static void setwater(int value){
+		water=value;
+		System.out.println(water);
+	}
+	
+	static int getwater(){
+		return (20);
+	}
+	static void setfluoride(String value){
+		fluoride=value;
+		System.out.println(fluoride);
+	}
+	
+	static String getfluoride(){
+		return ("sodium");
+	}
+	static void setweight(int value){
+		weight=value;
+		System.out.println(weight);
+	}
+	
+	static int getweight(){
+		return (900);
+	}
+	static void setclove(int value){
+		clove=value;
+		System.out.println(clove);
+	}
+	
+	static int getclove(){
+		return (50);
+	}
+	static void setneem(int value){
+		neem=value;
+		System.out.println(neem);
+	}
+	
+	static int getneem(){
+		return (20);
+	}
+	
+	static void setpudina(float value){
+		pudina=value;
+		System.out.println(pudina);
+	}
+	
+	static float getpudina(){
+		return (1.54f);
+	}
+	static void setmint(float value){
+		mint=value;
+		System.out.println(mint);
+	}
+	
+	static float getmint(){
+		return (1.67f);
+	}
+	static void setaloevera(int value){
+		aloevera=value;
+		System.out.println(aloevera);
+	
+	
+	static int getaloevera(){
+		return (30);
+	}
+	static void settulsi(byte value){
+		tulsi=value;
+		System.out.println(tulsi);
+	}
+	
+	static byte gettulsi();
+		return (20);
+	
+	static void setsalt(int value){
+		salt=value;
+		System.out.println(salt);
+	}
+	
+	static int getsalt(){
+		return (25);
+	}
+	
+	
+	
+}
+	
+	
+		
+		/**if(ref==67){
+		
+		System.out.println("true");
+		}
+		else
+		System.out.println("not print");
+	
+	   
+	  */
---
+++ ToothPasteRunner.java
@@ -0,0 +1,79 @@
+class ToothPasteRunner{
+	public static void main(String [] args){
+		System.out.println(price);
+		ToothPaste.setPrice(38d);
+		
+		double ref=ToothPaste.getPrice();
+		System.out.println(ref);
+	
+		
+
+		System.out.println(flavors);
+		ToothPaste.setflavors("MaxFresh");
+		
+		String ref1=ToothPaste.getflavors();
+		System.out.println(ref1);
+		
+		System.out.println(water);
+		ToothPaste.setwater(20);
+		
+		int ref3=ToothPaste.getwater();
+		System.out.println(ref3);
+		
+		System.out.println(fluoride);
+		ToothPaste.setfluoride("sodium");
+		
+		String ref4=ToothPaste.getfluoride();
+		System.out.println(ref4);
+		
+		System.out.println(weight);
+		ToothPaste.setweight();
+		
+		int ref5=ToothPaste.getweight();
+		System.out.println(ref5);
+		
+		System.out.println(clove);
+		ToothPaste.setclove(50);
+		
+		int ref6=ToothPaste.getclove();
+		System.out.println(ref6);
+		
+		System.out.println(neem);
+		ToothPaste.setneem(20);
+		
+		int ref7=ToothPaste.getneem();
+		System.out.println(ref7);
+		
+		System.out.println(pudina);
+		ToothPaste.setpudina(1.15f);
+		
+		float ref8=ToothPaste.getpudina();
+		System.out.println(ref8);
+		
+		System.out.println(mint);
+		ToothPaste.setmint(2.24f);
+		
+		float ref9=ToothPaste.getmint();
+		System.out.println(ref9);
+		
+		System.out.println(aloevera);
+		ToothPaste.setaloevera(20);
+		
+		int ref10=ToothPaste.getaloevera();
+		System.out.println(ref10);
+		
+		System.out.println(tulsi);
+		ToothPaste.settulsi(30);
+		
+		byte ref11=ToothPaste.gettulsi();
+		System.out.println(ref11);
+		
+		System.out.println(salt);
+		ToothPaste.setsalt(25);
+		
+		int ref12=ToothPaste.getsalt();
+		System.out.println(ref12);
+		
+		
+	}
+}
